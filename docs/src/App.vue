a959a41b9c2ad826afa37736230fefdb720cafc1
不惑 <717734797@qq.com>
2016-12-21 18:24:52 +0800

    '调整文档侧边大屏下样式'

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -63,7 +63,7 @@
       @media (min-width: 1281px) {
         top: 0;
         pointer-events: auto;
-        transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0) !important;
         box-shadow: $material-shadow-2dp;
       }
     }
@@ -99,7 +99,7 @@
       }
 
       img {
-        width: 160px;
+        width: 100px;
         margin-bottom: 16px;
       }
     }
